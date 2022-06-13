dff7574d7a6018595646cba36989b686940fa7a1
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-06-13 13:42:45 +0300

    Complete assignment

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -103,40 +103,185 @@ function homeTeamName() {
 function awayTeamName() {
     return gameObject().away.teamName
 }
+function numPointsScored(name) {
 
-function allKeys(obj,arrOfKeys) 
-{
-    if (typeof obj === "object") {
-       let newkeys = [...Object.keys(obj)]
-       for(const item of newkeys)
-       {
-        arrOfKeys.push(item)
-       }
-        for (const key of newkeys){
-            if(typeof(obj[key])==="object")
-            {
-                allKeys(obj[key],arrOfKeys)
-            }
-          
-        }   
+    if (teamNames("home").find(element => element === name)) {
+        return gameObject().home.players[name].points
+    }
+    else if (teamNames("away").find(element => element === name)) {
+        return gameObject().away.players[name].points
+    }
+    else {
+        return `${name} is not a player`
+    }
+}
+function ShoeSize(name) {
+    if (teamNames("home").find(element => element === name)) {
+        return gameObject().home.players[name].shoe
+    }
+    else if (teamNames("away").find(element => element === name)) {
+        return gameObject().away.players[name].shoe
+    }
+    else {
+        return `${name} is not a player`
     }
 
-    return arrOfKeys
 }
-function findSomething(obj,something)
-{
-    const keys=allKeys(obj,[])
-    return ` I found ${keys.find(item=>item===something)}`
+function teamColors(name) {
+    return gameObject()[name].colors
 }
-findSomething(gameObject(),"Brook Lopez")
-function numPointsScored(name) {
-    gameObject().find()
-    return points
-}
-function listPlayers(team)
-{
+
+function teamNames(team) {
     return Object.keys(gameObject()[team]["players"])
 }
+function playerNumbers(team) {
+    return teamNames(team).map(
+        player => gameObject()[team]["players"][player]["number"])
+}
+function playerStats(name) {
+    if (teamNames("home").find(player => player === name)) {
+        return gameObject().home.players[name]
+    }
+    else if (teamNames("away").find(player => player === name)) {
+        return gameObject().away.players[name]
+    }
+    else {
+        return `${name} is not a player`
+    }
+}
+function bigShoeRebounds() {
+    let shoeSizeHome = 0;
+    let shoeSizeAway = 0;
+    let homeT = teamNames("home").reduce(((string, player) => {
+        if (gameObject().home.players[player].shoe > shoeSizeHome) {
+            shoeSizeHome = gameObject().home.players[player].shoe
+            string = `${player}-${shoeSizeHome}`
+            return string
+        }
+        else {
+            return string
+        }
+    }), "")
+    let awayT = teamNames("away").reduce(((string, player) => {
+        if (gameObject().away.players[player].shoe > shoeSizeAway) {
+            shoeSizeAway = gameObject().away.players[player].shoe
+            string = `${player}-${shoeSizeAway}`
+            return string
+        }
+        else {
+            return string
+        }
+    }), homeT)
+
+    homeT = homeT.split("-")
+    awayT = awayT.split("-")
+
+
+    let mrBigshoe = parseInt(awayT[1], 10) > parseInt(homeT[1], 10) ? [awayT[0], "away"] : [homeT[0], "home"]
+    return gameObject()[mrBigshoe[1]].players[mrBigshoe[0]].rebounds
+
+}
+function mostPointsScored() {
+    let pointsHome = 0;
+    let pointsAway = 0;
+    let homeT = teamNames("home").reduce(((string, player) => {
+        if (gameObject().home.players[player].points > pointsHome) {
+            pointsHome = gameObject().home.players[player].points
+            string = `${player}-${pointsHome}`
+            return string
+        }
+        else {
+            return string
+        }
+    }), "")
+    let awayT = teamNames("away").reduce(((string, player) => {
+        if (gameObject().away.players[player].points > pointsAway) {
+            pointsAway = gameObject().away.players[player].points
+            string = `${player}-${pointsAway}`
+            return string
+        }
+        else {
+            return string
+        }
+    }), homeT)
+
+    homeT = homeT.split("-")
+    awayT = awayT.split("-")
+
+
+    let mrTopScorer = parseInt(awayT[1], 10) > parseInt(homeT[1], 10) ? awayT : homeT[0]
+    return `Top Scorer: ${mrTopScorer[0]}, ${mrTopScorer[1]} points`
+
+}
+function playerWithLongestName() {
+
+    let homeT = teamNames("home").reduce(((longestName, playerName) => {
+        if (playerName.length > longestName.length) {
+            longestName = playerName
+            return longestName
+        }
+        else {
+            return longestName
+        }
+    }), "")
+    let awayT = teamNames("away").reduce(((longestName, playerName) => {
+        if (playerName.length > longestName.length) {
+            longestName = playerName
+            return longestName
+        }
+        else {
+            return longestName
+        }
+    }), homeT)
+
+    return awayT
+}
+function doesLongNameStealATon()
+{
+    const longestName=playerWithLongestName()
+    let stealsHome = 0;
+    let stealsAway = 0;
+    let homeT = teamNames("home").reduce(((string, player) => {
+        if (gameObject().home.players[player].steals > stealsHome) {
+            stealsHome = gameObject().home.players[player].steals
+            string = `${player}-${stealsHome}`
+            return string
+        }
+        else {
+            return string
+        }
+    }), "")
+    let awayT = teamNames("away").reduce(((string, player) => {
+        if (gameObject().away.players[player].steals > stealsAway) {
+            stealsAway = gameObject().away.players[player].steals
+            string = `${player}-${stealsAway}`
+            return string
+        }
+        else {
+            return string
+        }
+    }), homeT)
+
+    homeT = homeT.split("-")
+    awayT = awayT.split("-")
+
+
+    let mrTopStealer = parseInt(awayT[1], 10) > parseInt(homeT[1], 10) ? awayT[0] : homeT[0]
+    return longestName==mrTopStealer
+
+}
+console.log(doesLongNameStealATon())
+console.log(playerWithLongestName())
+console.log(mostPointsScored())
+console.log(bigShoeRebounds())
+console.log(playerStats("Brook Lopez"))
+console.log(playerNumbers("away"))
+console.log(teamNames("away"))
+console.log(teamColors("away"))
+console.log(ShoeSize("Brook Lopez"))
+console.log(numPointsScored("Jeff Adrien"))
+
+
 function totalTeamPoints(team) {
     let theTeam = gameObject()[team]["players"]
     let accum = 0;
@@ -146,6 +291,7 @@ function totalTeamPoints(team) {
     }
     return accum
 }
+
 
 
 console.log(homeTeamName() + " vs " + awayTeamName())
